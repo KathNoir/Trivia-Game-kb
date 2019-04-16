ff32e24c1475514756a56a07c9b0bfc2b472e5d8
Blanco <0328414@students.dadeschools.net>
2019-04-16 14:15:57 -0400

    added things

---
+++ main.js
@@ -0,0 +1,23 @@
+var panel = $("#quiz-area");
+var countStartNumber = 30;
+
+//Question set
+var questions = [
+    
+    {
+    question: "What is Mario's last name?",
+    answers: ["Spaghetti, Mario, Jumpman, Luigi"],
+    correctAnswer: "Mario",
+    description: "His full name is Mario Jumpman Mario.",
+    image: "assets\mario.gif";
+    },
+
+    {
+    question: "What is the difference between Peach and Daisy in SSBU?",
+    answers: ["Melee is stronger for Peach, Melee is stronger for Daisy, No difference, All of the above"],
+    correctAnswer: "Melee is stronger for Peach",
+    description: "Although this is barely a noticable difference.",
+    image: "assets\mario.gif";
+    }
+
+]
