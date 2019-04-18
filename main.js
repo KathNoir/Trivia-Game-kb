1b87333dea0e5662db7810ac1a91d794e5a80586
Blanco <0328414@students.dadeschools.net>
2019-04-18 14:07:03 -0400

    added even more things

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var questions = [
     
     {
     question: "What is Mario's last name?",
-    answers: ["Spaghetti, Mario, Jumpman, Luigi"],
+    answers: ["Spaghetti", "Mario", "Jumpman", "Luigi"],
     correctAnswer: "Mario",
     description: "His full name is Mario Jumpman Mario.",
     image: "assets\mario.gif";
@@ -14,10 +14,48 @@ var questions = [
 
     {
     question: "What is the difference between Peach and Daisy in SSBU?",
-    answers: ["Melee is stronger for Peach, Melee is stronger for Daisy, No difference, All of the above"],
+    answers: ["Melee is stronger for Peach", "Melee is stronger for Daisy", "No difference", "All of the above"],
     correctAnswer: "Melee is stronger for Peach",
     description: "Although this is barely a noticable difference.",
-    image: "assets\mario.gif";
+    image: "assets\peach.gif";
     }
 
 ]
+
+var timer;
+
+var game = {
+    
+    questions: questions,
+    currentQuestion: 0,
+    counter: countStartNumber,
+    correct: 0,
+    incorrect: 0,
+
+    countdown: function() {
+        game.counter--;
+        $("#counter-number").html(game.counter);
+        if (gameCounter === 0) {
+            console.log("You ran out of time.");
+            game.timeUp();
+        }
+    },
+        
+    loadQuestion: function() {
+        timer = setInterval(game.countdown, 1000);
+        panel.html("<h2>" + questions[this.currentQuestion].question + "</h2>")
+        for (var i = 0; i < questions[this.currentQuestion].answer.length; i++); {
+            panel.append("<button class='answer-button' id='button' data-name=' " 
+            + questions[this.currentQuestion].answer[i] + "'>" 
+            + questions[this.currentQuestion].answers[i] + "</button>");
+        }
+    },
+
+        nextQuestion: function() {
+            game.counter = countStartNumber;
+            $("#counter-number").html(game.counter);
+            game.currentQuestion++;
+            game.loadQuestion();
+            
+        }
+}
