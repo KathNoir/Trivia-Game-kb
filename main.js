79800a7875d71120f5548bf7f5fc3f9b870bcadf
Blanco <0328414@students.dadeschools.net>
2019-04-23 13:44:49 -0400

    functions added 2

--- main.js
+++ main.js
@@ -57,5 +57,45 @@ var game = {
             game.currentQuestion++;
             game.loadQuestion();
             
+        },
+
+        timeUp: function() {
+            clearInterval(timer);
+
+            $('#counter-number').html(game.counter);
+
+            panel.html("<h2>You ran out of time!</h2>");
+
+            panel.append("<h3>The correct answer was" + $(questions[this.currentQuestion].correctAnswer) + "</h3>");
+
+            panel.append("<img src=' " + questions[this.currentQuestion].image + "'/>");
+
+            if (game.currentQuestion === questions.length - 1) {
+
+                setTimeout(game.results, 3000);
+
+            }
+            
+            else {
+
+                setTimeout(game.nextQuestion, 3000);
+
+            }
+
         }
+
+        results: function() {
+
+            clearInterval(timer);
+
+            panel.html("<h2> Here's how you did </h2>");
+
+            $("#counter-number").html(game.counter);
+
+            panel.append("<h3> Correct Answers: " + game.correct + "</h3>");
+            panel.append("<h3> Incorrect Answers: " + game.incorrect + "</h3>");
+            panel.append("<h3> Unanswered: " + (questions.length - (game.correct + game.incorrect)) + "</h3>");
+            panel.append("<br><button id='start-over'> Start Over? </button>");
+
+        },
 }
